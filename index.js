ea89de3843d90613e9d069139963ddbaab235f5b
Vaibhav Singh <vaibhav.singh@99Acres.com>
2021-02-11 08:05:31 +0530

    NEW COMMIT

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var btn = document.getElementById("share");
 btn.addEventListener("click", (e) => { 
 	if (navigator.share) {
 		  navigator.share({
-			  text: "Know all about Sector 78 on 99acres https://www.99acres.com/property-investment-in-sector-78-noida-piffid",
+			  text: "Know all about Sector 58 on 99acres https://www.99acres.com/property-investment-in-sector-78-noida-piffid",
+			  url: `https://www.99acres.com/property-investment-in-sector-78-noida-piffid`
 		  })
 			  .then(() => console.log('Successful share'))
 			  .catch((error) => console.log('Error sharing', error));
